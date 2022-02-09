d651a037d142cd39fc3e081f111b6ef8af64faa3
Max-Gabriel-Susman <gabe.susman@gmail.com>
2022-02-09 12:20:02 -0700

    haveing trouble posting to service on macOS, going to try in my ubuntu environment

--- handlers/products.go
+++ handlers/products.go
@@ -21,6 +21,13 @@ func (products *Products) ServeHTTP(responseWriter http.ResponseWriter, request 
 		products.getProducts(responseWriter, request)
 		return
 	}
+
+	if request.Method == http.MethodPost {
+		products.addProduct(responseWriter, request)
+		return
+	}
+	// catch all
+	responseWriter.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (products *Products) getProducts(responseWriter http.ResponseWriter, request *http.Request) {
@@ -31,4 +38,17 @@ func (products *Products) getProducts(responseWriter http.ResponseWriter, reques
 	}
 
 	responseWriter.Write(JSONData)
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.logger.Println("Handle POST Products")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.logger.Printf("Prod: %#v", prod)
 }
